e93bd22ff885ccc8c7b6848d5ecf79e895bdfd06
lucasDemori21 <lucas-demori2001@hotmail.com>
2026-05-28 20:27:08 -0300

    feat: tratamento de erros

--- zenon-fraud-detector/src/main/java/br/com/zenon/Transaction.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/Transaction.java
@@ -1,8 +1,19 @@
 package br.com.zenon;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public record Transaction(int step, TransactionType type, BigDecimal amount, TransactionCustomer origin,
                           TransactionCustomer recipient, boolean isFraud, boolean isFlaggedFraud) {
 
+    public Transaction {
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(amount);
+        Objects.requireNonNull(origin);
+        Objects.requireNonNull(recipient);
+
+        if (step <= 0) throw new IllegalArgumentException("O valor de step deve ser positivo: " + step);
+
+        if (amount.signum() < 0) throw new IllegalArgumentException("O valor de amount deve ser positivo: " + amount);
+    }
 }
