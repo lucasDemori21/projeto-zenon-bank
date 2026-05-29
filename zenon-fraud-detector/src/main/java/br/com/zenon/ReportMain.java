59af66013e3dba3e8e2c7b041a07c1b3152ef533
lucasDemori21 <lucas-demori2001@hotmail.com>
2026-05-28 23:48:05 -0300

    otimizacao de relatorio

---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/ReportMain.java
@@ -0,0 +1,16 @@
+package br.com.zenon;
+
+public class ReportMain {
+
+    void main () {
+        TransactionReport transactionReport = new TransactionReport();
+        TransactionReport.Statistics statistics = transactionReport.generateReport("data/PS_20174392719_1491204439457_log.csv");
+
+        IO.println("""
+            Total de linhas: %d
+            Total de fraudes: %d
+            Valor total transacionado: %.2f
+            """.formatted(statistics.totalTransaction(), statistics.totalFrauds(), statistics.totalAmount()));
+
+    }
+}
